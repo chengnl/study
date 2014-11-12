28a0e32ef1faf193a12b8f79db7948a38413ab27
chengnl <chengnl@chengnl-To-be-filled-by-O-E-M>
2014-11-12 12:03:14 +0800

    使用lock同步代码块

---
+++ src/main/java/syn/lock/Main.java
@@ -0,0 +1,22 @@
+package syn.lock;
+/**
+ *@author chengnl
+ *@E-mail:chengnengliang@vrvmail.com.cn
+ *@date 2014年11月12日 上午10:06:10
+ *@version 1.0
+ *@Description:TODO 类描述
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		PrintQueue   printQueue = new PrintQueue();
+		Thread thread[] = new Thread[10];
+		for(int i=0;i<10;i++){
+			thread[i] = new Thread(new Job(printQueue),"Thread"+i);
+		}
+		for(int i=0;i<10;i++){
+			thread[i].start();
+		}
+	}
+
+}
