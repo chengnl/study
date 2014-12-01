9aee527add83043f7d912277ef360642023e99df
chengnl <chengnl@chengnl-To-be-filled-by-O-E-M>
2014-12-01 17:35:33 +0800

    创建一个大小固定的线程执行者

---
+++ src/main/java/executor/lesson2/fixedexecutor/Server.java
@@ -0,0 +1,28 @@
+package executor.lesson2.fixedexecutor;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ *@author chengnl
+ *@E-mail:chengnengliang@vrvmail.com.cn
+ *@date 2014年12月1日 下午3:47:35
+ *@version 1.0
+ *@Description:服务
+ */
+public class Server {
+    private ThreadPoolExecutor executor;
+    public Server(){
+    	executor=(ThreadPoolExecutor) Executors.newFixedThreadPool(5);
+    }
+    public void executeTask(Task task){
+    	System.out.printf("Server: a new task has arrived .\n");		
+    	executor.execute(task);
+    	System.out.printf("Server: pool size  %d.\n",this.executor.getPoolSize());
+    	System.out.printf("Server: active count  %d.\n",this.executor.getActiveCount());
+    	System.out.printf("Server: task count: %d .\n",this.executor.getTaskCount());
+    }
+    public void endServer(){
+    	this.executor.shutdown();
+    }
+}
